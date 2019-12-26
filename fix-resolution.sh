df9a47bffe45ab8e7aee3ab6b8942efd8a566608
Ray Saltrelli <ray@saltrelli.com>
2019-12-26 13:24:07 -0500

    Updated for 3440x1440 resolution

--- fix-resolution.sh
+++ fix-resolution.sh
@@ -2,12 +2,12 @@
 # shows them that they actually can.
 
 # Create the new display mode
-xrandr --newmode "1920x1080"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
+xrandr --newmode "3440x1440" 419.50  3440 3696 4064 4688  1440 1443 1453 1493 -hsync +vsync
 
 # Add it to the table of possible resolutions
-xrandr --addmode DVI-I-1-1 1920x1080
+xrandr --addmode DVI-I-1-1 3440x1440
 xrandr --addmode eDP-1 1920x1080
 
 # Output the new display mode
-xrandr --output DVI-I-1-1 --mode 1920x1080
+xrandr --output DVI-I-1-1 --mode 3440x1440
 xrandr --output eDP-1 --mode 1920x1080
